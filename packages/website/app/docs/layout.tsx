6185ecb61ea416ca510f6faa838a025f81af8fc8
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2026-07-14 17:11:39 +0100

    fix: remove the fumadocs css import from the layout and put them in global.css

--- packages/website/app/docs/layout.tsx
+++ packages/website/app/docs/layout.tsx
@@ -1,4 +1,3 @@
-import 'fumadocs-ui/style.css';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import type { ReactNode } from 'react';
